733e14a42305646642a54f9c9bb4c9481d7a284d
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2023-03-07 17:25:45 +0100

    finalize topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,17 +1,42 @@
 import "./topbar.css";
-import { Search } from "@material-ui/icons";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 
 export default function Topbar() {
     return(
-        <div className="TopbarContainer">
-            <div className="topbarLeft"></div>
-                <span className="logo">Clownfb</span>
-            <div className="topbarCenter"></div>
-                <div className="searchBar">
-                    <Search/>
-                    <input placeholder="Search friend or post" className="searchInput"/>
-                </div> 
-            <div className="topbarRight"></div>
+        <div className="topbarContainer">
+        <div className="topbarLeft">
+          <span className="logo">Clownfb</span>
         </div>
-    )
+        <div className="topbarCenter">
+          <div className="searchbar">
+            <Search className="searchIcon" />
+            <input
+              placeholder="Search friend or post"
+              className="searchInput"
+            />
+          </div>
+        </div>
+        <div className="topbarRight">
+          <div className="topbarLinks">
+            <span className="topbarLink">Home</span>
+            <span className="topbarLink">Timeline</span>
+          </div>
+          <div className="topbarIcons">
+            <div className="topbarIconItem">
+              <Person />
+              <span className="topbarIconBadge">1</span>
+            </div>
+            <div className="topbarIconItem">
+              <Chat />
+              <span className="topbarIconBadge">3</span>
+            </div>
+            <div className="topbarIconItem">
+              <Notifications />
+              <span className="topbarIconBadge">2</span>
+            </div>
+          </div>
+          <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
+        </div>
+      </div>
+    );
 }
